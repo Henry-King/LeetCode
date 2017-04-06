73db73253df6b6bee319fbda4ada92afa6318fd7
YorkShen <shenyuancs@gmail.com>
2017-04-07 01:14:46 +0800

    O(n * M * LogM)

---
+++ python/week5/49.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        ret = {}
+        for item in strs:
+            key = str(sorted(list(item)))
+            if key in ret:
+                ret[key].append(item)
+            else:
+                ret[key] = [item]
+        return ret.values()
+
+
+s = Solution()
+print s.groupAnagrams(
+    ["hos", "boo", "nay", "deb", "wow", "bop", "bob", "brr", "hey", "rye", "eve", "elf", "pup", "bum", "iva", "lyx",
+     "yap", "ugh", "hem", "rod", "aha", "nam", "gap", "yea", "doc", "pen", "job", "dis", "max", "oho", "jed", "lye",
+     "ram", "pup", "qua", "ugh", "mir", "nap", "deb", "hog", "let", "gym", "bye", "lon", "aft", "eel", "sol", "jab"])
