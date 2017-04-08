a0c5b9e85a8b45eccbe6051494f71b1ce45ad1d3
YorkShen <shenyuancs@gmail.com>
2017-04-08 16:34:12 +0800

    O(n*n)

---
+++ python/week5/60.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        options = [i for i in xrange(1, n + 1)]
+        ret = []
+        factorials = [1]
+        for i in xrange(1, n - 1):
+            factorials.append(factorials[i - 1] * (i + 1))
+        place = k - 1
+
+        for i in xrange(len(factorials) - 1, -1, -1):
+            factorial = factorials[i]
+            index, place = divmod(place, factorial)
+            ret.append(str(options[index]))
+            del options[index]
+
+        if options:
+            ret.append(str(options[0]))
+        return "".join(ret)
+
+
+s = Solution()
+print s.getPermutation(1, 1)
