fbdc4b0ca8dd2e2311fdfcf8d60f59d3e8d0fcd2
YorkShen <shenyuancs@gmail.com>
2017-04-11 02:05:20 +0800

    Naive Approach based on C(n,k)= C(n,k-1) and C(n,0)=C(n,n)=1

--- python/week6/77.py
+++ python/week6/77.py
@@ -1,6 +1,3 @@
-import itertools
-
-
 class Solution(object):
     def combine(self, n, k):
         """
@@ -8,8 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        return list(itertools.combinations(xrange(1, n + 1), k))
+        if k == 0:
+            return [[]]
+        else:
+            if n - k < k:
+                return [[i for i in xrange(1, n + 1) if i not in item] for item in self.combine(n, n - k)]
+            else:
+                return [pre + [i] for i in xrange(1, n + 1) for pre in self.combine(i - 1, k - 1)]
 
 
 s = Solution()
-print s.combine(4, 3)
+print s.combine(3, 3)
