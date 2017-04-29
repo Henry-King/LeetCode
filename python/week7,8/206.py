12f40a6fc74e3bc1b26ccfd42d4de2a74afd0a4a
YorkShen <shenyuancs@gmail.com>
2017-04-29 15:25:23 +0800

    Update 206

--- python/week7,8/206.py
+++ python/week7,8/206.py
@@ -11,32 +11,7 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        if head:
-            new_head, new_tail = self.helper(head)
-            return new_head
-        else:
-            return None
-
-    def helper(self, head):
-        if head.next:
-            new_head, tail = self.helper(head.next)
-            tail.next = head
-            head.next = None
-            return new_head, head
-        else:
-            return head, head
-
-
-a = ListNode(1)
-b = ListNode(2)
-c = ListNode(3)
-d = ListNode(4)
-e = ListNode(5)
-a.next = b
-# b.next = c
-# c.next = d
-# d.next = e
-
-s = Solution()
-ret = s.helper(a)
-print ret
+        new_head = None
+        while head:
+            head.next, new_head, head = new_head, head, head.next
+        return new_head
