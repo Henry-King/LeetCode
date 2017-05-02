7eb5234c60543a7a65351e8dc9cf3561b2a1320b
YorkShen <shenyuancs@gmail.com>
2017-05-02 22:25:55 +0800

    Naive

---
+++ python/week9/101.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root:
+            if root.left and root.right:
+                return self.__helper(root.left, root.right)
+            elif not root.left and not root.right:
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def __helper(self, foo, bar):
+        if foo and bar:
+            if foo.val == bar.val:
+                return self.__helper(foo.left, bar.right) and self.__helper(foo.right, bar.left)
+            else:
+                return False
+        elif not foo and not bar:
+            return True
+        else:
+            return False
