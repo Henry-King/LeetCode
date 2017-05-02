120c33a50a6147877e57d32b7812635b8c4d2e3f
YorkShen <shenyuancs@gmail.com>
2017-05-03 00:53:13 +0800

    Update 9

--- python/week1/9.py
+++ python/week1/9.py
@@ -4,25 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0:
+        if x < 0 or (x != 0 and x % 10 == 0):
             return False
         else:
-            high = self.__highest_number(x)
-            low = 1
-            while low < high:
-                if (x / (10 ** (low-1))) % 10 == (x / (10 ** (high-1))) % 10:
-                    low += 1
-                    high -= 1
-                else:
-                    return False
-            return True
+            rev = 0
+            while x > rev:
+                rev = rev * 10 + x % 10
+                x /= 10
+            return x == rev or x == rev / 10
 
-    def __highest_number(self, x):
-        count = 1
-        while x >= 10:
-            x /= 10
-            count += 1
-        return count
 
 s = Solution()
 print s.isPalindrome(10)
