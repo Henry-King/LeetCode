54698d27a16bb8ccac3f695135ffb238e961c79f
YorkShen <shenyuancs@gmail.com>
2017-05-26 00:24:19 +0800

    Set

---
+++ python/week11/128.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = set(nums)
+        best = 0
+        for x in nums:
+            if x - 1 not in nums:
+                y = x + 1
+                while y in nums:
+                    y += 1
+                best = max(y - x, best)
+
+        return best
+
+
+s = Solution()
+print s.longestConsecutive([100, 4, 200, 1, 3, 2])
