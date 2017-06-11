6f75ef4a3c8fc98525779e17ea238cea51918a33
YorkShen <shenyuancs@gmail.com>
2017-06-11 20:20:25 +0800

    Digit Root
    https://en.wikipedia.org/wiki/Digital_root#Congruence_formula

---
+++ python/week13/258.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        return 1 + (num - 1) % 9 if num else 0
+
+
+s = Solution()
+print s.addDigits(38)
