ec170dbe9514239b67d0b99e9620b0ffa0102b3e
YorkShen <shenyuancs@gmail.com>
2017-07-03 00:09:32 +0800

    Math

---
+++ python/week15/319.py
@@ -0,0 +1,13 @@
+import math
+
+
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(math.sqrt(n))
+
+s = Solution()
+print s.bulbSwitch(3)
