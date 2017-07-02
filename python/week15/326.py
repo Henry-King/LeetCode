625e0c85a0d8ba9a4de1314184831794d0d8511b
YorkShen <shenyuancs@gmail.com>
2017-07-03 00:31:21 +0800

    Change the base of log.
    log(10, n)/log(10, 3) = log(3,n)

---
+++ python/week15/326.py
@@ -0,0 +1,14 @@
+import math
+
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return (math.log10(n) / math.log10(3)) % 1 == 0 if n > 0 else False
+
+
+s = Solution()
+print s.isPowerOfThree(4)
