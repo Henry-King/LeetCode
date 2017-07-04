3aaaf7d6afc59ac6f23cde0321ba6ad100d6dfb8
YorkShen <shenyuancs@gmail.com>
2017-07-04 22:59:49 +0800

    Math

---
+++ python/week15/338.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res = [0]
+        for i in xrange(1, num + 1):
+            res.append(res[i >> 1] + (i & 1))
+        return res
+
+
+s = Solution()
+print s.countBits(5)
