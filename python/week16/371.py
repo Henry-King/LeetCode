790066177239f3a755171e92587458c92ffe0b2d
YorkShen <shenyuancs@gmail.com>
2017-07-22 18:55:21 +0800

    Complement number

---
+++ python/week16/371.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        mask = 0xFFFFFFFF
+
+        while b != 0:
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        return a if a <= 0x7FFFFFFF else ~ (a ^ mask)
+
+
+s = Solution()
+print s.getSum(-1, 1)
