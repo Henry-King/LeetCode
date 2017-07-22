a824c3dd8e46f3ace937039cfe0a570cebb3a0e7
YorkShen <shenyuancs@gmail.com>
2017-07-22 17:39:16 +0800

    Math

---
+++ python/week16/367.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        i = 1
+        while num > 0:
+            num -= i
+            i += 2
+        return num == 0
+
+
+s = Solution()
+print s.isPerfectSquare(6)
