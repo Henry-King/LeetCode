3322f119320358a37cda11e7654277aeee80e040
YorkShen <shenyuancs@gmail.com>
2017-08-05 20:06:12 +0800

    Combination

--- python/week18/401.py
+++ python/week18/401.py
@@ -9,15 +9,14 @@ class Solution(object):
         """
         ret = []
         for i in xrange(num + 1):
-            ret.extend([a + ':' + b for a in self.hours(i) for b in self.minutes(num - i)])
+            ret.extend(['{}:{:0>2d}'.format(a, b) for a in self.hours(i) for b in self.minutes(num - i)])
         return ret
 
     def hours(self, on):
-        return [str(item) for item in filter(lambda value: value < 12, map(sum, combinations([1, 2, 4, 8], on)))]
+        return filter(lambda value: value < 12, map(sum, combinations([1, 2, 4, 8], on)))
 
     def minutes(self, on):
-        return [str(item).rjust(2, '0') for item in
-                filter(lambda value: value < 60, map(sum, combinations([1, 2, 4, 8, 16, 32], on)))]
+        return filter(lambda value: value < 60, map(sum, combinations([1, 2, 4, 8, 16, 32], on)))
 
 
 s = Solution()
